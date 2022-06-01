0750eedb8042b98fb8451886ca9fd2108aad18f4
bernacv <64037169+bernacv@users.noreply.github.com>
2022-06-01 16:56:15 +0100

    request username to api implemented

--- APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/MainActivity.java
+++ APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/MainActivity.java
@@ -2,30 +2,137 @@ package pt.ulisboa.tecnico.cmov.conversational_ist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPref;
+
+    private EditText nameEdt;
+    private Button postDataBtn;
+    private TextView responseTV;
+    private ProgressBar loadingPB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nameEdt = findViewById(R.id.editText1);
+        postDataBtn = findViewById(R.id.button1);
+        responseTV = findViewById(R.id.textView1);
+        loadingPB = findViewById(R.id.idLoadingPB);
+
         sharedPref = getPreferences(Context.MODE_PRIVATE);
+
+        postDataBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // validating if the text field is empty or not.
+                if (nameEdt.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter your username", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // calling a method to post the data and passing our name and job.
+                postDataUsingVolley(nameEdt.getText().toString());
+            }
+        });
+    }
+
+    private void postDataUsingVolley(String name) {
+        // url to post our data
+        String url = "https://cmuapi.herokuapp.com/api/users";
+        loadingPB.setVisibility(View.VISIBLE);
+
+        // creating a new variable for our request queue
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("name", name);
+
+        JSONObject jsonObj = new JSONObject(params);
+
+        // on below line we are calling a string
+        // request method to post the data to our API
+        // in this we are calling a post method.
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, jsonObj,new com.android.volley.Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // inside on response method we are
+                // hiding our progress bar
+                // and setting data to edit text as empty
+                loadingPB.setVisibility(View.GONE);
+                nameEdt.setText("");
+
+                // on below line we are displaying a success toast message.
+                Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
+                try {
+                    // on below line we are parsing the response
+                    // to json object to extract data from it.
+
+                    // below are the strings which we
+                    // extract from our json object.
+                    String name = response.getString("name");
+
+                    // on below line we are setting this string s to our text view.
+                    responseTV.setText("Name : " + name );
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // method to handle errors.
+                loadingPB.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
+                responseTV.setText("Username already exists!");
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // below line we are creating a map for
+                // storing our values in key and value pair.
+                Map<String, String> params = new HashMap<String, String>();
+
+                // on below line we are passing our key
+                // and value pair to our parameters.
+                params.put("name", name);
+
+                // at last we are
+                // returning our params.
+                return params;
+            }
+        };
+        // below line is to make
+        // a json object request.
+        queue.add(request);
     }
 
     public void login(View view) {
-
-        EditText username = (EditText)findViewById(R.id.editText1);
-
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(getString(R.string.saved_username), username.getText().toString());
+        editor.putString(getString(R.string.saved_username), nameEdt.getText().toString());
         editor.apply();
     }
 
