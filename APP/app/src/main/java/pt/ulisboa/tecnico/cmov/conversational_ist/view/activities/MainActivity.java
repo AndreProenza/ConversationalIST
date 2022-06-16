4f57e6d6acaa63644536d60c6472666710b8dbd1
AndreProenza <proenza.1999@gmail.com>
2022-06-16 14:48:11 +0100

    Fixed photo upload and added MainRoomAdapter

--- APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/view/activities/MainActivity.java
+++ APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/view/activities/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -26,8 +28,13 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
+import java.util.ArrayList;
+
 import pt.ulisboa.tecnico.cmov.conversational_ist.R;
+import pt.ulisboa.tecnico.cmov.conversational_ist.adapter.MainRoomsAdapter;
+import pt.ulisboa.tecnico.cmov.conversational_ist.adapter.RoomsAdapter;
 import pt.ulisboa.tecnico.cmov.conversational_ist.firebase.FirebaseHandler;
+import pt.ulisboa.tecnico.cmov.conversational_ist.model.Room;
 import pt.ulisboa.tecnico.cmov.conversational_ist.model.User;
 import pt.ulisboa.tecnico.cmov.conversational_ist.view.activities.profiles.MyProfileActivity;
 
@@ -41,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView userName;
 
     private FirebaseAuth mAuth;
+
+    RecyclerView recyclerView = null;
+    private ArrayList<Room> rooms;
+    MainRoomsAdapter roomsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +69,12 @@ public class MainActivity extends AppCompatActivity {
         isUserLoggedIn();
         init();
         initProfile();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycle_view_rooms);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //TODO
     }
 
     private void initProfile() {
