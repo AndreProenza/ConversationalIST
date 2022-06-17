4177070a94e090d582c61ac90721b474b96ffae5
AndreProenza <proenza.1999@gmail.com>
2022-06-17 15:26:39 +0100

    Fixed register v2

---
+++ APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/view/activities/RegisterFirebaseActivity.java
@@ -0,0 +1,189 @@
+package pt.ulisboa.tecnico.cmov.conversational_ist.view.activities;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import pt.ulisboa.tecnico.cmov.conversational_ist.R;
+import pt.ulisboa.tecnico.cmov.conversational_ist.firebase.FirebaseHandler;
+import pt.ulisboa.tecnico.cmov.conversational_ist.model.User;
+
+public class RegisterFirebaseActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
+
+    private TextInputEditText etRegEmail;
+    private TextInputEditText etRegPassword;
+    private TextInputEditText etRegUsername;
+    private TextView tvLoginHere;
+    private Button btnRegister;
+    private ProgressBar loadingPB;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference db;
+
+    private static boolean isUserRegistered;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_firebase);
+
+        sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+        etRegEmail = findViewById(R.id.etRegEmail);
+        etRegPassword = findViewById(R.id.etRegPass);
+        etRegUsername = findViewById(R.id.etRegUsername);
+        tvLoginHere = findViewById(R.id.tvLoginHere);
+        btnRegister = findViewById(R.id.btnRegister);
+        loadingPB = findViewById(R.id.idLoadingPB);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(view ->{
+            createUser();
+        });
+
+        tvLoginHere.setOnClickListener(view ->{
+            startActivity(new Intent(RegisterFirebaseActivity.this, pt.ulisboa.tecnico.cmov.conversational_ist.view.activities.LoginActivity.class));
+        });
+    }
+
+    private void createUser(){
+        String email = etRegEmail.getText().toString();
+        String password = etRegPassword.getText().toString();
+        String userName = etRegUsername.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            etRegEmail.setError("Email cannot be empty");
+            etRegEmail.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            etRegPassword.setError("Password cannot be empty");
+            etRegPassword.requestFocus();
+        }else if (TextUtils.isEmpty(userName)){
+            etRegPassword.setError("Username cannot be empty");
+            etRegPassword.requestFocus();
+        } else {
+            // calling a method to post the data and passing our name and job.
+            postDataUsingVolley(userName);
+            Log.d("IsUserRegistered = ", RegisterFirebaseActivity.isUserRegistered + "");
+            if (RegisterFirebaseActivity.isUserRegistered) {
+                Toast.makeText(RegisterFirebaseActivity.this, "Registration Error: User already exists", Toast.LENGTH_SHORT).show();
+            } else {
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            String userId = mAuth.getUid().toString();
+                            FirebaseHandler.registerUser(db, userId, userName);
+                            saveUsername(userName);
+                            Toast.makeText(RegisterFirebaseActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegisterFirebaseActivity.this, MainActivity.class));
+                        }else{
+                            Toast.makeText(RegisterFirebaseActivity.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }
+    }
+
+    private void postDataUsingVolley(String name) {
+        RegisterFirebaseActivity.isUserRegistered = false;
+
+        // url to post our data
+        String url = "https://cmuapi.herokuapp.com/api/users";
+        loadingPB.setVisibility(View.VISIBLE);
+
+        // creating a new variable for our request queue
+        RequestQueue queue = Volley.newRequestQueue(RegisterFirebaseActivity.this);
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("name", name);
+
+        JSONObject jsonObj = new JSONObject(params);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, jsonObj,new com.android.volley.Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                loadingPB.setVisibility(View.GONE);
+
+                //Toast.makeText(RegisterFirebaseActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
+                //saveUsername(name);
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                RegisterFirebaseActivity.isUserRegistered = true;
+                Log.d("isSuccessfull init = ", RegisterFirebaseActivity.isUserRegistered + "");
+                loadingPB.setVisibility(View.GONE);
+                String body;
+                //get status code here
+                String statusCode = String.valueOf(error.networkResponse.statusCode);
+                //get response body and parse with appropriate encoding
+                if(error.networkResponse.data!=null) {
+                    try {
+                        body = new String(error.networkResponse.data,"UTF-8");
+                        Toast.makeText(RegisterFirebaseActivity.this, "Error " + statusCode + " " + body, Toast.LENGTH_SHORT).show();
+                    } catch (UnsupportedEncodingException e) {
+                        Log.d("isSuccessfull exep = ", RegisterFirebaseActivity.isUserRegistered + "");
+                        RegisterFirebaseActivity.isUserRegistered = true;
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // yourMethod();
+            }
+        }, 2000);
+        // below line is to make
+        // a json object request.
+        queue.add(request);
+        Log.d("isSuccessfull final = ", RegisterFirebaseActivity.isUserRegistered + "");
+    }
+
+    public void saveUsername(String username) {
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("saved_username", username);
+        editor.apply();
+    }
+
+}
