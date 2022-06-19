650b2055be16998928de12826e5064c415e54d09
AndreProenza <proenza.1999@gmail.com>
2022-06-19 17:24:06 +0100

    Location added in add rooms

--- APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/view/activities/AddNewRoomActivity.java
+++ APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/view/activities/AddNewRoomActivity.java
@@ -2,21 +2,37 @@ package pt.ulisboa.tecnico.cmov.conversational_ist.view.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import pt.ulisboa.tecnico.cmov.conversational_ist.R;
 import pt.ulisboa.tecnico.cmov.conversational_ist.model.Room;
@@ -26,6 +42,11 @@ public class AddNewRoomActivity extends AppCompatActivity implements AdapterView
     private EditText edRoomName, edRoomDescription;
     private Spinner spinner;
     //private DatabaseReference db;
+    private TextView latitude, longitude, country, locality, address;
+    private TextView latitudeText, longitudeText, countryText, localityText, addressText;
+    private LinearLayout locationLayout;
+    private ProgressBar progressBar;
+    private FusedLocationProviderClient locationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +54,7 @@ public class AddNewRoomActivity extends AppCompatActivity implements AdapterView
         setContentView(R.layout.activity_add_new_room);
 
         init();
-        initSpinner();
+        initSpinnerAndLocation();
         initClick();
     }
 
@@ -43,15 +64,93 @@ public class AddNewRoomActivity extends AppCompatActivity implements AdapterView
         spinner = findViewById(R.id.sp_new_room);
     }
 
-    private void initSpinner() {
+    private void initSpinnerAndLocation() {
+        locationLayout = (LinearLayout) findViewById(R.id.location_ll);
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+        country = findViewById(R.id.country);
+        locality = findViewById(R.id.locality);
+        address = findViewById(R.id.address);
+        latitudeText = findViewById(R.id.latitude_coor);
+        longitudeText = findViewById(R.id.longitude_coor);
+        countryText = findViewById(R.id.country_coor);
+        localityText = findViewById(R.id.locality_coor);
+        addressText = findViewById(R.id.address_coor);
+
+        locationProvider = LocationServices.getFusedLocationProviderClient(this);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.room_visibility, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        locationLayout.setVisibility(View.GONE);
+                        break;
+                    case 1:
+                        locationLayout.setVisibility(View.GONE);
+                        break;
+                    case 2:
+                        locationLayout.setVisibility(View.VISIBLE);
+                        if (ActivityCompat.checkSelfPermission(AddNewRoomActivity.this,
+                                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                            getLocation();
+                        } else {
+                            ActivityCompat.requestPermissions(AddNewRoomActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                        }
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //spinner.setOnItemSelectedListener(this);
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationProvider.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+                    try {
+                        Geocoder geocoder = new Geocoder(AddNewRoomActivity.this, Locale.getDefault());
+                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                        //Set text to textviews
+                        latitudeText.setText(Double.toString(addresses.get(0).getLatitude()));
+                        longitudeText.setText(Double.toString(addresses.get(0).getLongitude()));
+                        countryText.setText(addresses.get(0).getCountryName());
+                        localityText.setText(addresses.get(0).getLocality());
+                        addressText.setText(addresses.get(0).getAddressLine(0));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     private void initClick() {
         findViewById(R.id.btn_back).setOnClickListener(view -> finish());
+        progressBar = findViewById(R.id.progress_bar);
         findViewById(R.id.btn_done).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,12 +164,16 @@ public class AddNewRoomActivity extends AppCompatActivity implements AdapterView
                     Toast.makeText(getApplicationContext(), "Please Select Room visibility", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    progressBar.setVisibility(View.VISIBLE);
                     //addRoomToFirebase();
                     //ADD HERE ROOM
+                    locationLayout.setVisibility(View.GONE);
+                    progressBar.setVisibility(View.GONE);
                 }
             }
         });
     }
+
 
     /**
     private void addRoomToFirebase() {
