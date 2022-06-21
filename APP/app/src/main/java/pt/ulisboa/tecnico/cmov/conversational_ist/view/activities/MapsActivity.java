9b6d90c7503921f22c46f7a2e6e226538905931a
António Martins <50891717+AL-CT@users.noreply.github.com>
2022-06-21 23:29:38 +0100

    Added search function to maps

--- APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/view/activities/MapsActivity.java
+++ APP/app/src/main/java/pt/ulisboa/tecnico/cmov/conversational_ist/view/activities/MapsActivity.java
@@ -1,18 +1,22 @@
 package pt.ulisboa.tecnico.cmov.conversational_ist.view.activities;
 
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
-import android.Manifest;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
-import android.media.Image;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,45 +25,48 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 import pt.ulisboa.tecnico.cmov.conversational_ist.R;
 import pt.ulisboa.tecnico.cmov.conversational_ist.databinding.ActivityMapsBinding;
-import pt.ulisboa.tecnico.cmov.conversational_ist.view.activities.profiles.MyProfileActivity;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
     private ImageButton send_map;
     private LatLng loc2send;
+    private SearchView map_searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        pt.ulisboa.tecnico.cmov.conversational_ist.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map_embed);
-        mapFragment.getMapAsync(this);
 
         send_map = findViewById(R.id.send_map);
         send_map.setVisibility(View.GONE);
 
-        send_map.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MapsActivity.this, RoomActivity.class);
-                intent.putExtra("message2send","https://www.google.com/maps/@"+loc2send.latitude+","+loc2send.longitude);
-                setResult(505,intent);
-                finish();
-            }
+        send_map.setOnClickListener(view -> {
+            Intent intent = new Intent(MapsActivity.this, RoomActivity.class);
+            intent.putExtra("message2send","https://www.google.com/maps/@"+loc2send.latitude+","+loc2send.longitude);
+            setResult(505,intent);
+            finish();
         });
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
         LocationManager lm = (LocationManager)getSystemService(LOCATION_SERVICE);
@@ -70,15 +77,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 15.0f));
         }
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener()
-        {
+        mMap.setOnMapClickListener(arg0 -> {
+            send_map.setVisibility(View.VISIBLE);
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(arg0).title("Marker"));
+            loc2send = arg0;
+        });
+
+        map_searchView = findViewById(R.id.map_searchView);
+
+        map_searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onMapClick(LatLng arg0)
-            {
-                send_map.setVisibility(View.VISIBLE);
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(arg0).title("Marker"));
-                loc2send = arg0;
+            public boolean onQueryTextSubmit(String query) {
+
+                String location = map_searchView.getQuery().toString();
+
+                List<Address> addressList = null;
+
+                if (location != null || location.equals("")) {
+
+                    Geocoder geocoder = new Geocoder(MapsActivity.this);
+                    try {
+
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if(addressList.size()>0) {
+                        Address address = addressList.get(0);
+
+                        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+                        send_map.setVisibility(View.VISIBLE);
+                        mMap.clear();
+
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+
+                        loc2send = latLng;
+
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f));
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        map_searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                map_searchView.setIconified(false);
             }
         });
     }
